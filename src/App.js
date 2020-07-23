e00d8d4a092a655f789797cda8e6fa68795e49e4
Ram Maheshwari <ram991011@gmail.com>
2020-07-23 13:10:19 +0530

    increased per page count

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends React.Component {
   state = {
     items: null,
     currPage: 1,
-    perPage: 10,
+    perPage: 25,
     pagLoader: false,
     searchLoader: false,
     searchTerm: '',
